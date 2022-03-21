1ae0c8f2cacd891853babe355ca33f68bd9ee883
pumkinzeeup <101658829+pumkinzeeup@users.noreply.github.com>
2022-03-22 00:20:13 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -173,7 +173,7 @@ function restart() {
     createjs.Tween.removeTweens ( bird );
     bird.x = startX;
     bird.y = startY;
-    bird.rotation = 90;
+    bird.rotation = 0;
     bird.gotoAndPlay("fly");
     createjs.Tween.get(bird, {loop:true}).to({y:startY + wiggleDelta}, 380, createjs.Ease.sineInOut).to({y:startY}, 380, createjs.Ease.sineInOut);
 }
